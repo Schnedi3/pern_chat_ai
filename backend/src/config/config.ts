8473c7a4d01af5629f72cfdf712ccd896c5c014a
Schnedi3 <schnedi3@gmail.com>
2024-11-03 12:30:57 +0100

    Backend: Integrate postgres database

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -2,4 +2,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.PORT;
+
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+export const PG_DATABASE = process.env.PG_DATABASE
+export const PG_HOST = process.env.PG_HOST
+export const PG_PASSWORD = process.env.PG_PASSWORD
+export const PG_PORT = process.env.PG_PORT
+export const PG_USER = process.env.PG_USER
