c190ae8c4abbd1e28cf7e9743d83494e1142509d
Schnedi3 <schnedi3@gmail.com>
2024-11-07 00:41:27 +0100

    Frontend: Display current chat in the sidebar

--- frontend/src/components/Nav/Sidebar.tsx
+++ frontend/src/components/Nav/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { Cancel01Icon, Delete02Icon } from "hugeicons-react";
 
+import { useChatStore } from "../../store/chatStore";
 import style from "./sidebar.module.css";
 
 interface IArchiveProp {
@@ -8,6 +9,7 @@ interface IArchiveProp {
 }
 
 export const Sidebar = ({ viewSidebar, setViewSidebar }: IArchiveProp) => {
+  const { chat } = useChatStore();
   // todo: replace with old chats
   const oldConversations = Array.from({ length: 4 });
 
@@ -24,10 +26,12 @@ export const Sidebar = ({ viewSidebar, setViewSidebar }: IArchiveProp) => {
 
       <ul className={style.chatList}>
         <h4 className={style.title}>Current chat</h4>
-        <li className={style.conversation}>
-          <p className={style.summary}>This is a test of the current chat</p>
-          <Delete02Icon className={style.deleteChatIcon} />
-        </li>
+        {chat.length !== 0 && (
+          <li className={style.conversation}>
+            <p className={style.summary}>{chat[0].content}</p>
+            <Delete02Icon className={style.deleteChatIcon} />
+          </li>
+        )}
       </ul>
 
       <ul className={style.chatList}>
